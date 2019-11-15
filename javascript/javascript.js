a936989316e24d47e62595f59af11dc1eb07ea99
DiegoSalazar27 <diegosalazargm@gmail.com>
2019-11-14 22:40:03 -0400

    Adding styles to cards

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,43 +1,104 @@
 
-var scroll = document.getElementById("scrollIndicator");
+// var scroll = document.getElementById("scrollIndicator");
 var body = document.getElementsByTagName("body")[0];
 
 //Variables para animación del form
-var i = 0;
-var form = document.getElementById("contatcForm");
-var submitButton = document.getElementById("submit");
+// var i = 0;
+// var form = document.getElementById("contatcForm");
+// var submitButton = document.getElementById("submit");
 
 
 window.addEventListener("load", function() {
-    var heightToScroll = Math.round(body.getBoundingClientRect().height - window.innerHeight);
-    form.addEventListener("animationend", THEANIMATION);
+    // var heightToScroll = Math.round(body.getBoundingClientRect().height - window.innerHeight);
+    // form.addEventListener("animationend", THEANIMATION);
 
-    window.addEventListener("scroll",  function () {
-        scrollindicator(heightToScroll);
-    });
+    // window.addEventListener("scroll",  function () {
+    //     scrollindicator(heightToScroll);
+    // });
 
-    scrollindicator(heightToScroll);
-    window.addEventListener("scroll", FormAnimationTrigger);
+    // scrollindicator(heightToScroll);
+    // window.addEventListener("scroll", FormAnimationTrigger);
+
+      (function () {
+  	'use strict';
+  	   var slides = document.querySelectorAll('.personContainer'),
+  		 button = document.getElementById('button'),
+  		 arrows = document.querySelectorAll('.lnr'),
+  		 carouselCount = 0,
+  		 scrollInterval,
+  		 interval = 10000;
+
+  	arrows[0].addEventListener('click', function (e) {
+  		e = e || window.event;
+  		e.preventDefault();
+  		carouselCount -= 100;
+  		slider();
+  		if (e.type !== 'autoClick') {
+  			clearInterval(scrollInterval);
+  			scrollInterval = setInterval(autoScroll, interval);
+  		}
+  	});
+  	arrows[1].addEventListener('click', sliderEvent);
+  	arrows[1].addEventListener('autoClick', sliderEvent);
+
+  	function sliderEvent(e) {
+  		e = e || window.event;
+  		e.preventDefault();
+  		carouselCount += 100;
+  		slider();
+  		if (e.type !== "autoClick") {
+  			clearInterval(scrollInterval);
+  			scrollInterval = setInterval(autoScroll, interval);
+  		}
+  	}
+
+  	function slider() {
+  		switch (carouselCount) {
+  			case -100:
+  				carouselCount = 0;
+  				break;
+  			case 300:
+  				carouselCount = 0;
+  				break;
+  			default:
+  				break;
+  		}
+  		console.log(carouselCount);
+  		for (var i = 0; i < slides.length; i += 1) {
+  			slides[i].setAttribute('style', 'transform:translateX(-' + carouselCount + '%)');
+  		}
+  	}
+
+  	// create new Event to dispatch click for auto scroll
+  	var autoClick = new Event('autoClick');
+  	function autoScroll() {
+  		arrows[1].dispatchEvent(autoClick);
+  	}
+
+  	// set timing of dispatch click events
+  	scrollInterval = setInterval(autoScroll, interval);
+
+  })();
 });
 
-function scrollindicator (heightToScroll) {
-    var scrollwidth = window.scrollY / heightToScroll;
-    scroll.style.transform = "scale(" + scrollwidth + ",1)";
-}
+// function scrollindicator (heightToScroll) {
+//     var scrollwidth = window.scrollY / heightToScroll;
+//     scroll.style.transform = "scale(" + scrollwidth + ",1)";
+// }
 
 
 
 //Funciones de animación de form
 
-function FormAnimationTrigger () {
-    if(form.getBoundingClientRect().top < 500) {
-        form.style.animationName = "test";
-        window.removeEventListener("scroll", FormAnimationTrigger);
-        form.addEventListener("animationend", THEANIMATION);
-    }
-}
-
-function THEANIMATION () {
-    (form.getElementsByClassName("basicInformationContainer"))[0].style.animationName = "animationLeftColumn";
-    (form.getElementsByClassName("textareaContainer"))[0].style.animationName = "animationRightColumn";
-}
+// function FormAnimationTrigger () {
+//     if(form.getBoundingClientRect().top < 500) {
+//         form.style.animationName = "test";
+//         window.removeEventListener("scroll", FormAnimationTrigger);
+//         form.addEventListener("animationend", THEANIMATION);
+//     }
+// }
+//
+// function THEANIMATION () {
+//     (form.getElementsByClassName("basicInformationContainer"))[0].style.animationName = "animationLeftColumn";
+//     (form.getElementsByClassName("textareaContainer"))[0].style.animationName = "animationRightColumn";
+// }
