7c7d7768edf46afa7cd3ec4046aef184c0176eea
Daniel Salazar <dnlsalazar27@gmail.com>
2019-11-30 21:33:10 -0400

    Working on work's section

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,6 +2,10 @@
 // var scroll = document.getElementById("scrollIndicator");
 var body = document.getElementsByTagName("body")[0];
 var root = document.documentElement;
+var filterButtons = document.getElementsByClassName("buttonFilter")
+const FILTER_LENGTH = filterButtons.length
+var works = document.getElementsByClassName("work")
+const WORKS_LENGTH = works.length
 
 //Variables para animación del form
 // var i = 0;
@@ -19,6 +23,11 @@ window.addEventListener("load", function() {
 
     // scrollindicator(heightToScroll);
     // window.addEventListener("scroll", FormAnimationTrigger);
+
+    for (let i = 0; i < FILTER_LENGTH; i++) {
+      filterButtons[i].addEventListener("click", workFilter)
+    }
+
 
   (function () {
   	'use strict';
@@ -94,3 +103,28 @@ window.addEventListener("load", function() {
 //     (form.getElementsByClassName("basicInformationContainer"))[0].style.animationName = "animationLeftColumn";
 //     (form.getElementsByClassName("textareaContainer"))[0].style.animationName = "animationRightColumn";
 // }
+
+
+function workFilter () {
+  const FILTER = this.getAttribute("filterType") === "all" ? null
+  : this.getAttribute("filterType")
+
+  for (let i = 0; i < WORKS_LENGTH; i++) {
+    if (!FILTER || FILTER === works[i].getAttribute("workType")) {
+      works[i].style.setProperty("display", "flex")
+      
+      setTimeout(() => {
+        works[i].style.setProperty("opacity", "1")
+        works[i].style.setProperty("transform", "scale(1)")
+      }, 500);
+    } else {
+        works[i].style.setProperty("opacity", "0")
+        works[i].style.setProperty("transform", "scale(0)")
+    
+        setTimeout(() => {
+          works[i].style.setProperty("display", "none")
+        }, 500);
+    }
+
+  }
+}
